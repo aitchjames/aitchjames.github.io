6663a6c7d8ca4781ea9ddce6519ff7533a58a2ae
Hayden <hayden@aitch.co>
2020-09-02 17:54:43 +1000

    redesigned the aitch.co portfolio page

---
+++ Desktop/aitchjames.github.io-master/js/index.js
@@ -0,0 +1,29 @@
+let darkmode = localStorage.getItem('darkmode')
+const darkmodeToggle = document.querySelector('#dark-mode-toggle')
+
+// Dark Mode Toggle Functions & Event Listeners
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'enabled')
+}
+  
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.setItem('darkmode', null)
+}
+   
+if (darkmode === 'enabled') {
+    enableDarkmode()
+}
+  
+darkmodeToggle.addEventListener('click', () => {
+    darkmode = localStorage.getItem('darkmode')
+    
+    if (darkmode !== 'enabled') {
+      enableDarkmode()
+    } else {  
+      disableDarkmode(); 
+    }
+});
+  
